a25e34fe9f1e5516239e33208fd59615204cc67c
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2018-06-02 21:30:17 -0400

    Switching to the new tiles

--- Assets/Scripts/MapTiles.cs
+++ Assets/Scripts/MapTiles.cs
@@ -3,9 +3,8 @@ using UnityEngine;
 
 class MapTiles : MonoBehaviour {
     private int width = 512, height = 512;
-    public Texture2D[] asphalt, diagonalLeft, diagonalRight, floor, grass,
-           linear, linearStop, linearStopBoth, linearStopLeft,
-           threeWayLeft, threeWayRight;
+    public Texture2D[] asphalt, curveLeft, curveRight, empty, floor, fourWay,
+                       grass, straight, threeWayLeft, threeWayRight;
 
     public void Instantiate (string[][] tiles) {
         int sizeX = tiles[0].Length, sizeY = tiles.Length;
@@ -17,99 +16,71 @@ class MapTiles : MonoBehaviour {
         for (int y = 0; y < sizeY; y++) {
             for (int x = 0; x < sizeX; x++) {
                 texture.SetPixels(x * width, y * height, width, height,
-                        GetTilePixels(tiles[y][x]));
+                                  GetPixels(tiles[y][x]));
             }
         }
         texture.Apply();
 
         GetComponent<MeshRenderer>().material.mainTexture = texture;
-
-        // FIXME Might need to update the box collider too
-        // https://docs.unity3d.com/ScriptReference/BoxCollider-size.html
-        // https://answers.unity.com/questions/32618/changing-box-collider-size.html
     }
 
-    private Color[] GetTilePixels(string tile) {
+    private Color[] GetPixels(string tile) {
         var pixels = new Color[width * height];
 
-        // FIXME Rotat e images so that you have a uniform if/else for rotation
-        string[] tileDetails = tile.Split('/'); // XXX Rename
-        switch (tileDetails[0])
-        {
+        var tileDetails = tile.Split('/'); // XXX
+        switch (tileDetails[0]) {
             case "asphalt":
                 pixels = asphalt.PickRandom().GetPixels();
                 break;
-            case "diag_left":
-                pixels = diagonalLeft.PickRandom().GetPixels();
-                if (tileDetails[1] == "W")
+            case "curve_left":
+                pixels = curveLeft.PickRandom().GetPixels();
+                if (tileDetails[1] == "N")
+                    pixels = RotatePixels(pixels, true);
+                else if (tileDetails[1] == "S")
+                    pixels = RotatePixels(pixels, false);
+                else if (tileDetails[1] == "W")
                     Array.Reverse(pixels);
-                if (tileDetails[1] == "N")
-                    pixels = RotatePixels(pixels, true);
-                if (tileDetails[1] == "S")
-                    pixels = RotatePixels(pixels, false);
                 break;
-            case "diag_right":
-                pixels = diagonalRight.PickRandom().GetPixels();
+            case "curve_right":
+                pixels = curveRight.PickRandom().GetPixels();
                 if (tileDetails[1] == "N")
                     pixels = RotatePixels(pixels, false);
-                if (tileDetails[1] == "S")
+                else if (tileDetails[1] == "S")
                     pixels = RotatePixels(pixels, true);
-                // TODO West or East
+                else if (tileDetails[1] == "E") // XXX
+                    Array.Reverse(pixels);
+                break;
+            case "empty":
+                //pixels = empty.PickRandom().GetPixels();
                 break;
             case "floor":
                 pixels = floor.PickRandom().GetPixels();
                 break;
+            case "4way":
+                pixels = fourWay.PickRandom().GetPixels();
+                break;
             case "grass":
                 pixels = grass.PickRandom().GetPixels();
                 break;
-            case "linear":
-                pixels = linear.PickRandom().GetPixels();
+            case "straight":
+                pixels = straight.PickRandom().GetPixels();
                 if (tileDetails[1] == "E")
-                    pixels = RotatePixels(pixels, true);
-                if (tileDetails[1] == "W")
                     pixels = RotatePixels(pixels, false);
-                if (tileDetails[1] == "S")
-                    Array.Reverse(pixels);
-                break;
-            case "linear_stop":
-                pixels = linearStop.PickRandom().GetPixels();
-                if (tileDetails[1] == "E")
+                else if (tileDetails[1] == "W")
                     pixels = RotatePixels(pixels, true);
-                if (tileDetails[1] == "W")
-                    pixels = RotatePixels(pixels, false);
-                if (tileDetails[1] == "S")
-                    Array.Reverse(pixels);
-                break;
-            case "linear_stop_both":
-                pixels = linearStopBoth.PickRandom().GetPixels();
-                if (tileDetails[1] == "E")
-                    pixels = RotatePixels(pixels, true);
-                if (tileDetails[1] == "W")
-                    pixels = RotatePixels(pixels, false);
-                if (tileDetails[1] == "S")
-                    Array.Reverse(pixels);
-                break;
-            case "linear_stop_left":
-                pixels = linearStopLeft.PickRandom().GetPixels();
-                if (tileDetails[1] == "E")
-                    pixels = RotatePixels(pixels, true);
-                if (tileDetails[1] == "W")
-                    pixels = RotatePixels(pixels, false);
-                if (tileDetails[1] == "S")
-                    Array.Reverse(pixels);
                 break;
             case "3way_left":
                 pixels = threeWayLeft.PickRandom().GetPixels();
-                if (tileDetails[1] == "E")
-                    pixels = RotatePixels(pixels, false);
-                if (tileDetails[1] == "W")
-                    pixels = RotatePixels(pixels, true);
                 if (tileDetails[1] == "S")
                     Array.Reverse(pixels);
+                else if (tileDetails[1] == "E")
+                    pixels = RotatePixels(pixels, false);
+                else if (tileDetails[1] == "W")
+                    pixels = RotatePixels(pixels, true);
                 break;
             case "3way_right":
                 pixels = threeWayRight.PickRandom().GetPixels();
-                // TODO All 3 out of 4 positions
+                // XXX N or S or E or W
                 break;
         }
 
