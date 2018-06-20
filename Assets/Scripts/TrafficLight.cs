74a4366b0dc189a1701b45e2c7f3611d753afb0f
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2018-06-19 23:50:25 -0400

    Basic traffic light script

---
+++ Assets/Scripts/TrafficLight.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLight : MonoBehaviour {
+    public int frequency = 5;
+    public Light[] lights1; // XXX
+    public Light[] lights2; // XXX
+
+    void Start () {
+        StartCoroutine(FirstPattern());
+    }
+
+    IEnumerator FirstPattern() {
+        foreach (var light in lights1)
+            light.color = new Color(0, 1, 0, 1);
+        foreach (var light in lights2)
+            light.color = new Color(1, 0, 0, 1);
+        yield return new WaitForSeconds(frequency);
+        StartCoroutine(SecondPattern());
+    }
+
+    IEnumerator SecondPattern() {
+        foreach (var light in lights2)
+            light.color = new Color(0, 1, 0, 1);
+        foreach (var light in lights1)
+            light.color = new Color(1, 0, 0, 1);
+        yield return new WaitForSeconds(frequency);
+        StartCoroutine(FirstPattern());
+    }
+}
